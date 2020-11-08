0ef02b36690ae1f015a58115345b2d7010e45b1c
Cam T. Bui <ncbui@users.noreply.github.com>
2020-11-07 20:47:40 -0800

    Updates

---
+++ precache-manifest.c65a91ce7176088053f6ea6a713eff79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c48fad8918e38523cf96b519d72785e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26fc09b6751d31a5b357",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "40ee7563a7ac59f82f48",
+    "url": "/static/css/main.6cea5e54.chunk.css"
+  },
+  {
+    "revision": "26fc09b6751d31a5b357",
+    "url": "/static/js/2.42f3d06b.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/static/js/2.42f3d06b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ee7563a7ac59f82f48",
+    "url": "/static/js/main.429320dd.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9d97f1c6cf579d26ebeef7d46697e170",
+    "url": "/static/media/0.9d97f1c6.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ncbui.github.io/precache-manifest.22ab20ac2708149a9ea9874878412116.js"
+  "/precache-manifest.c65a91ce7176088053f6ea6a713eff79.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ncbui.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
