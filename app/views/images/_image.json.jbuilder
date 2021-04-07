fdcba94e6266a6d99c079cdfdac3769181eef55c
ncbui <ncbui@users.noreply.github.com>
2021-04-06 21:01:04 -0700

    Generate scaffold for Image

---
+++ app/views/images/_image.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! image, :id, :src, :project_id, :created_at, :updated_at
+json.url image_url(image, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :images
   resources :projects
   resources :abouts
   resources :contacts
